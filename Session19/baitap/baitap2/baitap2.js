b1229f0e3548895f0d7606bc01f66738241b05db
nguyen03hu <nguyen33hu@gmail.com>
2025-03-03 22:23:51 +0700

    bai10

--- Session19/baitap/baitap2/baitap2.js
+++ Session19/baitap/baitap2/baitap2.js
@@ -1,8 +1,9 @@
-let inputChar = prompt("Nhập một ký tự:");
-let alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
-if (alphabet.indexOf(inputChar) !== -1) {
-    alert(`${inputChar} là chữ cái.`);
+let input = prompt("Nhập một ký tự hoặc chuỗi:");
+if (input.indexOf("0") !== -1 || input.indexOf("1") !== -1 || input.indexOf("2") !== -1 ||
+    input.indexOf("3") !== -1 || input.indexOf("4") !== -1 || input.indexOf("5") !== -1 ||
+    input.indexOf("6") !== -1 || input.indexOf("7") !== -1 || input.indexOf("8") !== -1 ||
+    input.indexOf("9") !== -1) {
+    document.write(`Chuỗi "${input}" không phải là chữ cái`);
 } else {
-    alert(`${inputChar} không phải là chữ cái.`);
+    document.write(`Chuỗi "${input}" là chữ cái`);
 }
